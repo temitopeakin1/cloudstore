22fcbc376d5a846654571df880df3472c3aa4d3b
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-11-09 14:01:37 +0100

    initial-commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins", 
+});
+
+export const metadata: Metadata = {
+  title: "CloudStore - Store and upload files at a breeze",
+  description: "Store files on the cloud with zero worries",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${poppins.variable} font-poppins antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl">
+        CloudStore - Store and upload files at a breeze
+      </h1>
+    </div>
+  );
+}
